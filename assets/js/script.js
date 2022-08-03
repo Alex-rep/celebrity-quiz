a92a8c4af6f7a7287e812fb036bdbd4160955a42
Alex-rep <nuclearCMP_t1058@protonmail.com>
2022-08-03 12:03:21 +0000

    Add Array of Questions and Answers plus event listener checking if the DOM is loaded

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+const questionArray = [
+    {
+        question: "Who plays Galadriel in 'Lord of the Rings'?",
+        answer: "Kate Blanchett",
+        option1: "Kate Blanchett",
+        option2: "Sharon Stone"
+    },
+    {
+        question: "Who plays Nathan Algren in 'Last Samurai'?",
+        answer: "Tom Cruise",
+        option1: "Brad Pitt",
+        option2: "Tom Cruise"
+    },
+    {
+        question: "Who plays Black Widow in 'Marvel's Avengers'?",
+        answer: "Scarlett Johansson",
+        option1: "Scarlett Johansson",
+        option2: "Natalie Portman"
+    },
+    {
+        question: "Who plays Harry S. Stamper in 'Armageddon'?",
+        answer: "Bruce Willis",
+        option1: "Russell Crowe",
+        option2: "Bruce Willis"
+    },
+    {
+        question: "Who plays Maximus in 'Gladiator'?",
+        answer: "Russell Crowe",
+        option1: "Russell Crowe",
+        option2: "Ben Affleck"
+    }
+]
+
+
+window.addEventListener('DOMContentLoaded', (event) =>{
+
+    console.log(questionArray[2]);
+    
+
+
+
+
+
+
+
+})
