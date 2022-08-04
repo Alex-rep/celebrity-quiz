64c8064330fcc76f8548e6d1eb0a0a3d47c89088
Alex-rep <nuclearCMP_t1058@protonmail.com>
2022-08-04 13:42:50 +0000

    Add Event Listener code, nextQuestion function and relative comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//this is the array of objects containing the questions and answers for the quiz
 const questionArray = [
     {
         question: "Who plays Galadriel in 'Lord of the Rings'?",
@@ -28,18 +29,62 @@ const questionArray = [
         answer: "Russell Crowe",
         option1: "Russell Crowe",
         option2: "Ben Affleck"
+    },
+    {
+        question: "Congrats, you completed the quiz!",
+        answer: "end",
+        option1: "Restart",
+        option2: "Restart"
     }
 ]
 
-
+//main game loop starts after DOM is loaded
 window.addEventListener('DOMContentLoaded', (event) =>{
 
-    const buttons = document.getElementsByClassName('buttons');
+    //load the buttons and question paragraph into JS variables
+    const buttons = Array.from(document.getElementsByClassName('buttons'));
     const question = document.getElementById('question');
+    const correct = document.getElementById('correct');
+    const incorrect = document.getElementById('incorrect');
+    let que_index = 0;
+   
+    loadQuestion(que_index);
+    
+    for(let index = 0; index < buttons.length; index++){
+        buttons[index].addEventListener('click', function (event) {
+            nextQuestion(event);
+        });
+    }
 
-    loadQuestion(1);
 
+    function nextQuestion(event){
+        
+        btn_answ = event.target.innerText;
 
+        if(btn_answ === 'Restart'){
+            
+            que_index = 0;
+            document.getElementById('correct').innerText = '0';
+            document.getElementById('incorrect').innerText = '0';
+            loadQuestion(que_index);
+
+        }else if(btn_answ === questionArray[que_index].answer){
+
+            let score = parseInt(document.getElementById('correct').innerText);
+            document.getElementById('correct').innerText = ++score;
+            loadQuestion(++que_index);
+        
+        }else{
+
+            let unScore = parseInt(document.getElementById('incorrect').innerText);
+            document.getElementById('incorrect').innerText = ++unScore;
+            loadQuestion(++que_index);
+        }
+    }
+    /**
+     * Loads the next question and possible answers for the buttons from the
+     * questionArray object array 
+     */
     function loadQuestion(index){
 
         question.innerHTML = questionArray[index].question;
